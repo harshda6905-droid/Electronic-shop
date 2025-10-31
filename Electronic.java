0558c29212cd33b4dd81417006f07d734d216dfe
harshda6905-droid <harshda6905@gmail.com>
2025-10-31 20:41:25 +0530

    Create Electronic.java

---
+++ Electronic.java
@@ -0,0 +1,15 @@
+package com.example.electronicstore.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+    private String category;
+
+    // Getters and Setters
+}
